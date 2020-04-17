2d5470f6966fa1d15d5948edc4b456f9d6af6634
Jeremy Moore <alt.photosbyjeremy@gmail.com>
2020-04-17 09:38:12 -0400

    validate images
    create batch_jhove_jp2

--- jhove_jp2.py
+++ jhove_jp2.py
@@ -1,5 +1,7 @@
-from pathlib import Path
+uifrom pathlib import Path
 
+# because of the call out to the shell using '!', this can't be ran normally
+# ADD a run call, so this can be scripted
 # functions
 def delete_dot_files(directory):
     dir_path = Path(directory)
@@ -17,11 +19,13 @@ def jhove_jp2(directory):
     jp2_path_list = sorted(dir_path.glob('**/*.jp2'))
     delete_dot_files(dir_path)
     jp2_path_list = sorted(dir_path.glob('**/*.jp2'))
+    number_of_jp2 = len(jp2_path_list)
 
     # instantiate error dictionary
     error_dict = {}
-    for jp2_path in jp2_path_list:
-        print(jp2_path.name)
+    for i, jp2_path in enumerate(jp2_path_list):
+        if i % 10 == 0:
+            print(f'processing image: {jp2_path.name}\nimage {i+1} of {number_of_jp2}')
         status = !{jhove} "{str(jp2_path)}" | grep -e 'Status:'
         status = status[0]
         if status == '  Status: Well-Formed and valid':
@@ -32,5 +36,13 @@ def jhove_jp2(directory):
     return error_dict
 
 # variables
-root_path = Path('/Users/dlisla/Pictures/athletics-programs/Output')
+root_path = Path('/Users/dlisla/Pictures/athletics-programs/Output/toJhove')
 jhove = f'~/jhove/jhove -m JPEG2000-hul'
+
+def batch_process(root_path):
+    dir_path_list = sorted([x for x in root_path.iterdir() if x.is_dir()])
+    for dir_path in dir_path_list:
+        print(f'processing {dir_path} . . . ')
+        errors = jhove_jp2(dir_path)
+        if errors:
+            print(f'errors: {errors}')
